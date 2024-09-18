e088d32efeb51fbb9f1cf13ff26500e0e80d388e
Francesca Vera <sally.acorn16@hotmail.com>
2024-09-17 21:43:31 -0500

    simulacro de clase dungeon master
     creación de las bases para que la clase dungeon master después lea las listas de enemigos y de items respectivamente
    definiendo las estructuras y la inicialización necesarias para gestionar el juego. Las listas de enemigos e items se inicializan como vacías y se llenarán con valores más adelante en el código(eso espero)

--- promedio 1/Program.cs
+++ promedio 1/Program.cs
@@ -6,10 +6,39 @@ using System.Threading.Tasks;
 
 namespace promedio_1
 {
+    // Clase principal que ejecuta el programa
     internal class Program
     {
         static void Main(string[] args)
         {
+            // se crea la instancia de dungeon master para crear el juego
+            DungeonMaster dungeonMaster = new DungeonMaster();
+            // Ejecuta el menú principal del juego
+            dungeonMaster.Execute();
         }
     }
-}
+
+    // Clase DungeonMaster, crear mundo y enemigos
+    internal class DungeonMaster
+    {
+        // Jugador que será controlado por el usuario
+        private Player player;
+        // Lista de enemigos en el juego
+        private List<Enemy> enemies;
+        // Lista de items disponibles en el juego
+        private List<Item> items;
+
+        // Constructor que inicializa las listas y el jugador
+        public DungeonMaster()
+        {
+            player = new Player();  // Crea el jugador
+            enemies = new List<Enemy>();  // Crea la lista de enemigos
+            items = new List<Item>();  // Crea la lista de items
+        }
+    }
+
+        //private player player; hace referencia a que estoy creando una variable llamada player,de tipo player 
+
+
+    //clase 
+    
